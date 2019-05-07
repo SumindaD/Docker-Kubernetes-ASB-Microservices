24db5a8f487e5b0e3f17a99e90dcc44d430b19d9
Suminda De Silva <sumindaniro@gmail.com>
2019-05-07 13:28:40 +0530

    Added VersionController

---
+++ Audit/Audit/Controllers/VersionController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Audit.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        // GET api/version
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Version 1";
+        }
+    }
+}
